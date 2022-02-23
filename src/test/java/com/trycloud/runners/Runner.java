0467f7dbd1c0abd662d1eff59bd50548e2f6a7b3
mhaydarov <h.merdan@yahoo.com>
2022-02-23 19:11:59 +0000

    Initial commit

---
+++ src/test/java/com/trycloud/runners/Runner.java
@@ -0,0 +1,19 @@
+package com.trycloud.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                  "html:target/cucumber-report.html"},
+        features = "src/test/resources/features",
+        glue = "com/zerobank/step_definitions",
+        dryRun = false,
+        tags = "@smoke"
+)
+public class Runner {
+
+}
