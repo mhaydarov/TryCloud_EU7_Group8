9d504ca57ad6e72d01c18021efce87447164dce5
aysultan <aysltns@gmail.com>
2022-02-24 21:06:47 +0000

    some changes by Aysoltan

--- src/test/java/com/trycloud/pages/temp.java
+++ src/test/java/com/trycloud/pages/temp.java
@@ -3,6 +3,7 @@ package com.trycloud.pages;
 public class temp {
     public static void main(String[] args) {
         System.out.println(" let's work togheter Ashfar");
+        System.out.println("Aysoltans work on process");
 
     }
 }
