f108f537097bccf146fa633d6023c5458f7203d0
aysultan <aysltns@gmail.com>
2022-03-02 18:02:29 +0000

    some changes by Aysoltan

--- src/test/java/com/trycloud/runners/Runner.java
+++ src/test/java/com/trycloud/runners/Runner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
+        strict = true,
         tags = "@contacts" //"@wip"
 )
 public class Runner {
