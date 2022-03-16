2fe60088be1ffb4c9b250beaa174c48476eb9ea3
0firat <ofirat2019@gmail.com>
2022-03-16 16:44:12 +0300

    I am on the stage

---
+++ src/test/java/com/trycloud/step_definitions/EditNeFileStepDefs.java
@@ -0,0 +1,59 @@
+package com.trycloud.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class EditNeFileStepDefs {
+    @Given("the user is on the home page")
+    public void the_user_is_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks the folder button right top of the dashboard page")
+    public void user_clicks_the_folder_button_right_top_of_the_dashboard_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks the plus sign at the {string} page")
+    public void user_clicks_the_plus_sign_at_the_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("select the upload file button")
+    public void select_the_upload_file_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify the upload successfully achieved")
+    public void verify_the_upload_successfully_achieved() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks the three dots of the selected folder or file")
+    public void user_clicks_the_three_dots_of_the_selected_folder_or_file() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("select the {string} option")
+    public void select_the_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify copying process done successfully")
+    public void verify_copying_process_done_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify the that the file deleted")
+    public void verify_the_that_the_file_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
